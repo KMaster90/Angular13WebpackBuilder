7acd5fbd82f6ea337b6e421f7c3012d000082da5
KMaster90 <capuz90@gmail.com>
2022-04-26 16:26:41 +0200

    angular 13 gira su webpack

---
+++ extra-webpack.config.ts
@@ -0,0 +1,19 @@
+import { Configuration, DefinePlugin } from 'webpack';
+import { version } from '@project';
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+
+/**
+ * This is where you define your additional webpack configuration items to be appended to
+ * the end of the webpack config.
+ */
+export default {
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'footer.html',
+      template: 'src/footer.template.html',
+    }),
+    new DefinePlugin({
+      APP_VERSION: JSON.stringify(version),
+    }),
+  ],
+} as Configuration;
---
+++ func-webpack.config.ts
@@ -0,0 +1,26 @@
+import { Configuration, DefinePlugin } from 'webpack';
+import { CustomWebpackBrowserSchema, TargetOptions } from '@angular-builders/custom-webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import { version } from '@project';
+
+/**
+ * This is where you define a function that modifies your webpack config
+ */
+export default (
+  cfg: Configuration,
+  opts: CustomWebpackBrowserSchema,
+  targetOptions: TargetOptions
+) => {
+  if(cfg.plugins){
+    cfg.plugins.push(
+      new HtmlWebpackPlugin({
+        filename: 'footer.html',
+        template: 'src/footer.template.html',
+      }),
+      new DefinePlugin({
+        APP_VERSION: JSON.stringify(version),
+      })
+    );
+  }
+  return cfg;
+};
